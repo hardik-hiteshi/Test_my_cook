f4a1c98daab5559e60f8ea06b3b43875c5fe6282
Amber.naqvi <amber.naqvi@hiteshi.com>
2023-09-15 13:03:14 +0530

    created get last & by range api

--- src/domains/news/news.service.ts
+++ src/domains/news/news.service.ts
@@ -6,6 +6,7 @@ import {
 import { CreateNewsDto, UpdateNewsDto } from './dtos';
 import { NewsDocument } from './schema/news.schema';
 import { NewsRepository } from './repository/news.repository';
+import { UserDocument } from '../user/schema/user.schema';
 
 @Injectable()
 export class NewsService {
@@ -15,10 +16,6 @@ export class NewsService {
     body: CreateNewsDto,
     region: string,
   ): Promise<NewsDocument> {
-    const news = await this.newsRepo.findOne(body.niceName, region);
-
-    if (news) throw new BadRequestException('news already exist');
-
     return await this.newsRepo.createOne(body, region);
   }
 
@@ -50,5 +47,58 @@ export class NewsService {
   public async deleteOne(region: string, niceName: string): Promise<void> {
     const news = await this.newsRepo.deleteOne(region, niceName);
     if (!news) throw new NotFoundException('news not found');
+  }
+
+  public async deleteImage(region: string, niceName: string): Promise<void> {
+    const news = await this.newsRepo.findOne(niceName, region);
+
+    if (!news) throw new NotFoundException('news not found');
+
+    news.image = [];
+
+    await news.save();
+  }
+
+  public async getNewsByDateRange(
+    dates: string,
+    user: UserDocument,
+    region: string,
+    profiles?: string,
+  ): Promise<NewsDocument[]> {
+    const date = new Date(dates);
+    if (isNaN(date.getTime())) throw new BadRequestException();
+
+    const profile = profiles || 'machine';
+
+    const news = await this.newsRepo.findNewsByDateRange(
+      region,
+      date,
+      user,
+      profile,
+    );
+    if (news.length <= 0) throw new NotFoundException('news not found');
+
+    user.profile.lastViewNews = new Date();
+    await user.save();
+
+    return news;
+  }
+
+  public async getLastNews(
+    last: number,
+    region: string,
+    profiles?: string,
+  ): Promise<NewsDocument> {
+    const profile = profiles || 'machine';
+
+    const where = { enabled: true };
+
+    where['media.' + profile] = true;
+
+    const news = await this.newsRepo.findLastNews(region, profile, last);
+
+    if (!news) throw new NotFoundException('news not found');
+
+    return news;
   }
 }
